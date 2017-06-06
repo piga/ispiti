ff9d825a5568d8f49ade521267e85c6bcc415ab4
Darko Vulin <darkopmfst@gmail.com>
2017-06-06 20:22:10 +0000

    sign-up finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   resources :zadataks
   
   root "zadataks#index"
   
   get 'tags/:tag', to: 'zadataks#index', as: "tag"
+  
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
