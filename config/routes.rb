18fe6004bdc6cafe62aff4e334ce4f72727b99c8
Darko Vulin <darkopmfst@gmail.com>
2017-06-12 12:38:37 +0000

    account activation half finished

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   
   resources :users
+  
+  resources :account_activations, only: [:edit]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
