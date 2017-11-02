87dece2988a330aea43cb2901397a99154b682db
Darko Vulin <darkopmfst@gmail.com>
2017-11-02 16:07:08 +0000

    changed postgres database encoding to utf8, and aded some zadataks in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,3 +42,12 @@ zadataks = Zadatak.order(:created_at).take(2)
   content = Faker::Lorem.sentence(5)
   zadataks.each { |zadatak| zadatak.comments.create!(autor: autor, content: content) }
 end
+
+#peraićevi zadatci
+problem = 'Dokaži   $\sqrt{1+i} = \sqrt{\frac{\sqrt{2}+1}{2}} + \sqrt{\frac{\sqrt{2}-1}{2}} \: i$.'
+solution = '$$\begin{align}\sqrt{1+i} &= \sqrt{\frac{\sqrt{2}+1}{2}} +\sqrt{\frac{\sqrt{2}-1}{2}} \: i\quad  \Big/^2\\\\1+i&=\frac{\sqrt2+1}{2}+2\sqrt{\frac{\sqrt2+1}{2}\cdot{}\frac{\sqrt2-1}{2}}i-\frac{\sqrt2-1}{2}\\\\1+i&=\frac{\sqrt2+1-\sqrt2+1}{2}+\frac{2i}{2}\sqrt{(\sqrt2+1)(\sqrt2-1)}\\\\1+i&=1+i\sqrt{2-\sqrt2+\sqrt2-1}\\\\1+i&=1+i.\end{align}$$'
+Zadatak.create(problem: problem, solution: solution).all_tags=('dokaži jednakost, korjeni')
+
+problem = 'Ako je $f(x)=x^3-1$, $g(x)=x^3+1$, koliko je $f(a+1)-g(a-1)$?'
+solution = '$$\begin{align}&f(x)=x^3-1, g(x)=x^3+1,\\\\&f(a+1)-g(a-1)=(a+1)^3-1-(a-1)^3-1\\\\&=a^3+3a^2+3a+1-1-a^3+3a^2-3a+1-1=6a^2.\end{align}$$'
+Zadatak.create(problem: problem, solution: solution).all_tags=('funkcije')
