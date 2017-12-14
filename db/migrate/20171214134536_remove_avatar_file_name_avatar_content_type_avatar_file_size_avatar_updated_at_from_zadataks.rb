376315aa113d6f79a01f3caac2e3c437b737fbaf
Darko Vulin <darkopmfst@gmail.com>
2017-12-14 15:22:11 +0000

    deleted migration because heroku problems

--- db/migrate/20171214134536_remove_avatar_file_name_avatar_content_type_avatar_file_size_avatar_updated_at_from_zadataks.rb
+++
@@ -1,8 +0,0 @@
-class RemoveAvatarFileNameAvatarContentTypeAvatarFileSizeAvatarUpdatedAtFromZadataks < ActiveRecord::Migration
-  def change
-    remove_column :zadataks, :avatar_file_name, :string
-    remove_column :zadataks, :avatar_content_type, :string
-    remove_column :zadataks, :avatar_file_size, :integer
-    remove_column :zadataks, :avatar_updated_at, :datetime
-  end
-end
