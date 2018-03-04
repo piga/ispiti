d795febe85f7d026b67b27b0da40ddc7d0b9c23e
Darko Vulin <darkopmfst@gmail.com>
2018-03-04 13:41:50 +0000

    again trying

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
     preserve_files: true,
     url: ':s3_domain_url',
     path: '/:class/:attachment/:id_partition/:style/:filename',
-    s3_host_name: 's3-eu-frankfurt.amazonaws.com',
+    s3_host_name: 's3.eu-central-1.amazonaws.com',
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
