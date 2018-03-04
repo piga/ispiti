cf05d4ee1912e31bf43cc05128bec2b41dd471d6
Darko Vulin <darkopmfst@gmail.com>
2018-03-04 14:05:39 +0000

    maybe working paperclip and s3 and heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,8 +96,6 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     preserve_files: true,
-    url: ':s3_domain_url',
-    path: '/:class/:attachment/:id_partition/:style/:filename',
     s3_host_name: 's3.eu-central-1.amazonaws.com',
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
